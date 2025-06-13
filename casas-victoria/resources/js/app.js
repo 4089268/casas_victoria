60476d7e24e0e036e4c46ecf56f0b59a348fbe70
Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2025-06-13 16:44:42 -0600

    Changes

--- casas-victoria/resources/js/app.js
+++ casas-victoria/resources/js/app.js
@@ -8,6 +8,10 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import Toast from "vue-toastification";
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+import AnimateOnScroll from 'primevue/animateonscroll';
 
 const toast_options = {
     transition: "Vue-Toastification__bounce",
@@ -17,6 +21,24 @@ const toast_options = {
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{cyan.50}',
+            100: '{cyan.100}',
+            200: '{cyan.200}',
+            300: '{cyan.300}',
+            400: '{cyan.400}',
+            500: '{cyan.500}',
+            600: '{cyan.600}',
+            700: '{cyan.700}',
+            800: '{cyan.800}',
+            900: '{cyan.900}',
+            950: '{cyan.950}'
+        }
+    }
+});
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
@@ -25,6 +47,12 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .use(Toast, toast_options)
+            .use(PrimeVue, {
+                theme: {
+                    preset: MyPreset
+                }
+            })
+            .directive('animateonscroll', AnimateOnScroll)
             .mount(el);
     },
     progress: {
